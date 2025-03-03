4c868a2f277f6b315596f3b3e30fadf5fd282467
kaichengchang <36093201+a48412437@users.noreply.github.com>
2025-03-03 21:14:34 +0800

    Add files via upload

---
+++ lab4/gen.py
@@ -0,0 +1,46 @@
+import random
+import numpy as np
+import os
+
+input_path = "input.txt"
+PAT_NUM = 1
+with open(input_path,'w') as f:
+
+    for pat_count in PAT_NUM:
+
+        f.write(pat_count+'\n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# x_file = open(input_path_x,'w+')
+# h_file = open(input_path_h,'w+')
+# u_file = open(input_path_u,'w+')
+# pat_num_
+
+
+
+
+
+
+# x_file.close()
+# h_file.close()
+# u_file.close()
